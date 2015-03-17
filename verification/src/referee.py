107ebaf6582e65711012d60dfc81aeee2d24bcf8
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-17 10:12:22 +0300

    fix golf

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,12 +1,13 @@
 from checkio_referee import RefereeBase
-from checkio_referee.covercode import py_unwrap_args
-from checkio_referee.validators import FloatEqualValidator
+from checkio_referee import covercodes, validators, representations
 
 import settings
 import settings_env
 from tests import TESTS
 
-FloatEqualValidator.PRECISION = 1
+Validator = validators.FloatEqualValidator
+
+Validator.PRECISION = 1
 
 
 class Referee(RefereeBase):
@@ -14,9 +15,14 @@ class Referee(RefereeBase):
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
     FUNCTION_NAME = "distance"
-    VALIDATOR = FloatEqualValidator
+    VALIDATOR = Validator
     ENV_COVERCODE = {
-        "python_2": py_unwrap_args,
-        "python_3": py_unwrap_args,
+        "python_2": covercodes.py_unwrap_args,
+        "python_3": covercodes.py_unwrap_args,
         "javascript": None
+    }
+    CALLED_REPRESENTATIONS = {
+        "python_2": representations.py_tuple_representation,
+        "python_3": representations.py_tuple_representation,
+        "javascript": representations.py_tuple_representation
     }
